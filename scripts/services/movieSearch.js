222cafd79cf44be4f9d427beeae97609cd859a62
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-07-26 19:45:16 -0600

    removed broken code section

--- scripts/services/movieSearch.js
+++ scripts/services/movieSearch.js
@@ -1,25 +1,17 @@
-// routerApp.factory('shareDataService', ["$http",  function($http) {
-//
-//   var moviesFound = ['hey'];
-//
-//   var moveMovies = function() {
-//       moviesFound.push(newObj);
-//   };
-//   var getMovies = function(){
-//       return moviesFound;
-//   };
-//   return {
-//     moveMovies: moveMovies,
-//     getMovies: getMovies
-//   };
-//
-//
-// }]);
+routerApp.factory('shareDataService', ["$http",  function($http) {
 
-routerApp.factory('shareDataService', function() {
-    return {
-        talk: function(what) {
-            return "Say " + what;
-        }
-    }
-});
+  var moviesFound = [];
+
+  var moveMovies = function() {
+      moviesFound.push(newObj);
+  };
+  var getMovies = function(){
+      return moviesFound;
+  };
+  return {
+    moveMovies: moveMovies,
+    getMovies: getMovies
+  };
+
+
+}]);

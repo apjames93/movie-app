38ba6c9b222babac8761a0f79862b9281d7697a2
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-08-06 12:04:04 -0600

    done

--- scripts/controllers/MovieController.js
+++
@@ -1,5 +0,0 @@
-routerApp.controller('MovieController', ["$scope", "$http",  "shareDataService", function($scope, $http, $stateParams, shareDataService) {
-  $scope.test = "winning";
-  // $scope.showmovies = shareDataService.getMovies();
-
-  }]);
